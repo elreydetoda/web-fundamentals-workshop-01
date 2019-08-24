01a6ced880eb9ccdfaaae026cec1a9a1776391f4
Fabio Nolasco <gomidefabio@gmail.com>
2019-08-24 08:05:57 -0400

    Better colors

--- main.js
+++ main.js
@@ -1,20 +1,27 @@
+/* eslint-disable no-unused-vars */
 function getPeople() {
+  const ul = document.createElement('ul');
 
-    const ul = document.createElement('ul');
+  fetch('https://swapi.co/api/people')
+    .then((resp) => resp.json())
+    .then((people) => {
+      people.results.forEach((person) => {
+        const li = document.createElement('li');
+        li.innerHTML = `
+                <section>
+                    <header>
+                        <a href="https://en.wikipedia.org/wiki/${person.name}" target="_blank">
+                            <h1>${person.name}</h1>
+                        </a>
+                    </header>
+                    <summary>
+                        <p>Eye color: ${person.eye_color}</p>
+                        <p>Hair color: ${person.hair_color}</p>
+                    </summary>
+                </section>`;
+        ul.appendChild(li);
+      });
 
-    fetch('https://swapi.co/api/people')
-        .then(resp => resp.json())
-        .then(people => {
-
-            people.results.forEach(person => {
-                const li = document.createElement('li');
-                li.innerHTML = `<a 
-                href="https://en.wikipedia.org/wiki/${person.name}"
-                target="_blank">${person.name}</a>`;
-                ul.appendChild(li);
-            });
-
-            document.body.appendChild(ul);
-        });
-
+      document.body.appendChild(ul);
+    });
 }
